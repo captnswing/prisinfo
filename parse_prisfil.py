17736fcc64c05747d2dce7e815daee5f7fc2a299
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2013-10-06 22:27:27 +0200

    parsing title and price from magento_prisfil

--- parse_prisfil.py
+++ parse_prisfil.py
@@ -3,6 +3,8 @@
 import os
 import urllib2
 from settings import *
+import csv
+import StringIO
 
 
 def get_prisfil_csv():
@@ -13,13 +15,21 @@ def get_prisfil_csv():
     if not os.path.exists(PRISFIL_CACHED):
         print "fetching prisfil from {0:s}...".format(PRISFIL_URL)
         csvdata = urllib2.urlopen(PRISFIL_URL).read()
-        with open(PRISFIL_CACHED, 'w') as prisfil:
+        with open(PRISFIL_CACHED, 'wb') as prisfil:
             prisfil.write(csvdata)
     else:
         print "using cached prisfil '{0:s}'...".format(PRISFIL_CACHED)
-        csvdata = open(PRISFIL_CACHED).read()
-    return csvdata
+        csvdata = open(PRISFIL_CACHED, 'rb').read()
+    return StringIO.StringIO(csvdata)
 
 
 if __name__ == '__main__':
     csv_data = get_prisfil_csv()
+    reader = csv.DictReader(csv_data, delimiter='\t', quotechar='"')
+    for i, row in enumerate(reader):
+        if i == 0:
+            print row.keys()
+            print
+        if not row['price']:
+            continue
+        print "{0:.<80s}{1:.>5.2f}kr".format(row['title'], float(row['price']))
