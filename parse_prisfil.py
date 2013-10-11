bce38364ed6bacbb0db161a7fad7c7c1ae73ba66
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2013-10-11 23:47:41 +0200

    refactored main()

--- parse_prisfil.py
+++ parse_prisfil.py
@@ -73,24 +73,29 @@ class UnicodeDictReader:
         return self
 
 
+def get_prisfil_data(cached):
+    # get csv data
+    csv_data = get_prisfil_csv(cached)
+    # parse csv data
+    reader = UnicodeDictReader(csv_data, delimiter='\t', quotechar='"')
+    # extract interesting fields
+    prisfil_data = []
+    for i, row in enumerate(reader):
+        if not row['price']:
+            row['price'] = -1
+        prisfil_data.append((row['title'], float(row['price'])))
+    return prisfil_data
+
+
 if __name__ == '__main__':
     # create argument and options parser
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--cached", help="use locally cached prisfil",
                         action="store_true")
     args = parser.parse_args()
-    # get csv data
-    csv_data = get_prisfil_csv(args.cached)
-    # parse csv data
-    reader = UnicodeDictReader(csv_data, delimiter='\t', quotechar='"')
-    # extract interesting fields
-    prisfil_data = []
-    i = 0
-    for i, row in enumerate(reader):
-        if not row['price']:
-            row['price'] = -1
-        prisfil_data.append((row['title'], float(row['price'])))
+    # get data
+    prisfil_data = get_prisfil_data(args.cached)
     # pretty print result
-    print "\nparsed {0:d} products".format(i)
+    print "\nparsed {0:d} products".format(len(prisfil_data))
     for t, p in sorted(prisfil_data, key=itemgetter(1)):
         print u"{0:.<90s}{1:.>10.0f}kr".format(t, p)
