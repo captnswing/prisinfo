743fc7f47f7c4bb4eedc3b6b82e5655cc0cb1d91
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2013-10-11 23:49:45 +0200

    first stab at flask admin gui

---
+++ admin.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from parse_prisfil import get_prisfil_data, UnicodeDictReader
+
+# configuration
+#DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+# create our little application
+app = Flask(__name__)
+app.config.from_object(__name__)
+Bootstrap(app)
+
+@app.route("/")
+def hello():
+    entries = get_prisfil_data(cached=True)
+    return render_template('show_entries.html', entries=entries)
+
+if __name__ == "__main__":
+    app.run()
