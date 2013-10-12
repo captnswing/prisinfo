d2d9e75705376d4c55542f2e5a41a619cac744e1
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2013-10-12 16:31:23 +0200

    renamed template

--- admin.py
+++ admin.py
@@ -5,11 +5,7 @@ from flask_bootstrap import Bootstrap
 from parse_prisfil import get_prisfil_data, UnicodeDictReader
 
 # configuration
-#DATABASE = '/tmp/flaskr.db'
 DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'admin'
 
 # create our little application
 app = Flask(__name__)
@@ -19,7 +15,7 @@ Bootstrap(app)
 @app.route("/")
 def hello():
     entries = sorted(get_prisfil_data(cached=True))
-    return render_template('show_entries.html', entries=entries)
+    return render_template('prisfil.html', entries=entries)
 
 if __name__ == "__main__":
     app.run()
