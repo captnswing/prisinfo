485071666d69a5d08a610948d75595e280e63e04
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2013-10-27 19:32:18 -0400

    displaying product info now as well

--- magentoclient.py
+++ magentoclient.py
@@ -8,14 +8,7 @@ import xmlrpclib
 from settings import XMLRPC_URL
 import os
 import sys
-
-
-def display_available_resources():
-    print "-" * 120
-    for r in mclient.resources(sessionid):
-        for m in r['methods']:
-            print "{:<50} {}".format(m['path'], m['title'])
-        print "-" * 120
+import json
 
 
 if __name__ == '__main__':
@@ -26,12 +19,34 @@ if __name__ == '__main__':
         print "environment variables MAGENTO_API_USER and MAGENTO_API_KEY need to be set."
         sys.exit(-1)
 
+    #-----------------------------------------------
+    # start session
+    #-----------------------------------------------
     mclient = xmlrpclib.ServerProxy(XMLRPC_URL)
     sessionid = mclient.login(api_user, api_key)
 
-    display_available_resources()
+    #-----------------------------------------------
+    # displaying all available resources
+    #-----------------------------------------------
+    for r in mclient.resources(sessionid):
+        for m in r['methods']:
+            print "{:<50} {}".format(m['path'], m['title'])
+        print "-" * 120
+
+    #-----------------------------------------------
+    # displaying list of all products
+    #-----------------------------------------------
     prodlist = mclient.call(sessionid, "catalog_product.list")
     for p in prodlist:
-        print "{:<30} {}".format(p['sku'], p['name'].encode('utf8'))
+        print u"{:<40} {}".format(p['sku'], p['name'])
 
+    #-----------------------------------------------
+    # displaying product info
+    #-----------------------------------------------
+    prodlist = mclient.call(sessionid, "catalog_product.info", ("50085703", "", "", "sku")  )
+    print json.dumps(prodlist, indent=4)
+
+    #-----------------------------------------------
+    # end session
+    #-----------------------------------------------
     mclient.endSession(sessionid)
