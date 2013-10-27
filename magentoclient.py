0395ef85628489ecd9a17cab7766357099070096
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2013-10-27 14:52:16 -0400

    listing all accessible resources and methods

--- magentoclient.py
+++ magentoclient.py
@@ -13,8 +13,14 @@ import sys
 def xmlrpc_test(api_user, api_key):
     proxy = xmlrpclib.ServerProxy(XMLRPC_URL)
     sessionid = proxy.login(api_user, api_key)
-    print sessionid
-    print proxy.resources(sessionid)
+    resources = proxy.resources(sessionid)
+
+    for r in resources:
+        print r['name']
+        for m in r['methods']:
+            print "{:<50} {}".format(m['path'], m['title'])
+        print
+
     proxy.endSession(sessionid)
 
 
@@ -23,6 +29,6 @@ if __name__ == '__main__':
         api_user = os.environ['MAGENTO_API_USER']
         api_key = os.environ['MAGENTO_API_KEY']
     except KeyError:
-        print "environment variables MAGENTO_API_USER and MAGENTO_API_KEY not found."
+        print "environment variables MAGENTO_API_USER and MAGENTO_API_KEY need to be set."
         sys.exit(-1)
     xmlrpc_test(api_user, api_key)
